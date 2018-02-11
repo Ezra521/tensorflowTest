68311c684c31088540b1c29b9e5d12dbc8d928cf
zgahwuqiankun@qq.com <au52185879@>
2018-02-12 00:20:53 +0800

    tensorflow实现验证码识别的前期基础

--- test4.py
+++ test4.py
@@ -17,11 +17,18 @@ print(sess.run(loss,{x:[1,2,3,4],y:[0,-1,-2,-3]})) #打印损失函数的值
 
 #我们可以通过给W、b再分配值来改进模型。一个变量的初始值由tf.Variable提供，但可以用tf.assign来改变：
 #这里是我手动找到w，b
-fixW = tf.assign(W,[-1.])
-fixb = tf.assign(b,[1.])
-sess.run([fixW,fixb])
-print(sess.run(loss,{x:[1,2,3,4],y:[0,-1,-2,-3]}))
+# fixW = tf.assign(W,[-1.])
+# fixb = tf.assign(b,[1.])
+# sess.run([fixW,fixb])
+# print(sess.run(loss,{x:[1,2,3,4],y:[0,-1,-2,-3]}))
 
+#这里使用tensorflow提供的api梯度下降来实现
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+sess.run(init)
+for i in range(1000):
+    sess.run(train,{x:[1,2,3,4],y:[0,-1,-2,-3]})
+print(sess.run([W,b]))
 
 #保存tensorboard图
 writer = tf.summary.FileWriter('D:/ten', tf.get_default_graph())
---
+++ test6.py
@@ -0,0 +1,45 @@
+import random
+import string
+import sys
+import math
+from PIL import Image,ImageDraw,ImageFont,ImageFilter
+
+#用来绘制干扰线
+def gene_line(draw):
+    py = random.randint(0, 3)
+    begin = (-py*50, random.randint(0, 100))
+    end = (200-py*50, random.randint(0, 100))
+    draw.line([begin, end], fill = 0,width=5)
+
+filename="D:/"
+typen=2
+typet=["arialbd.ttf","corbelb.ttf"]
+#字体的位置
+font_path = "C:/Windows/Fonts/"
+#生成验证码图片的高度和宽度
+size = (180,180)
+#生成验证码
+def gene_code():
+    width,height = size #宽和高
+    image = Image.new('1',(width,height),1) #创建图片
+    font = ImageFont.truetype(font_path+typet[random.randint(0, typen-1)],70) #验证码的字体
+    draw = ImageDraw.Draw(image)  #创建画笔
+    source = ['0','1','2','3','4','5','6','7','8','9']
+    text = random.randint(0, 9) #生成字符串
+    font_width, font_height = font.getsize(source[text])
+    draw.text(((width - font_width) /2, (height - font_height)/2-5),source[text],font= font,fill=0) #填充字符串
+    rx=random.uniform(-0.2, 0.2)
+    ry=random.uniform(-0.2, 0.2)
+    image = image.transform((width+100,height+100), Image.AFFINE, (1,rx,0,ry,1,0),Image.BILINEAR)  #创建扭曲
+    px=random.randint(-20, 20)
+    py=random.randint(-6, 6)
+    image = image.crop((65-70*rx+py,40-70*ry+px,115-70*rx+py,140-70*ry+px))
+    aa = str(".png")
+    path = filename + source[text] + aa
+    image.save(path)
+    draw = ImageDraw.Draw(image)
+    gene_line(draw)
+    gene_line(draw)
+    image.save(path)
+
+gene_code()
